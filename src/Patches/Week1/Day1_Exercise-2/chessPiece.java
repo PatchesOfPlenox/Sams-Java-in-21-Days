45e9d6555baef114eb9615535a24aab1bad9520d
Patches S <teh.bloo.box@gmail.com>
2011-10-30 21:43:23 -0500

    My entry for exercise question 2.

---
+++ src/Patches/Week1/Day1_Exercise-2/chessPiece.java
@@ -0,0 +1,14 @@
+/*
+ * This is not intended to be a working program, just an POC.
+ * The book didn't tell me to write code for it, but I figure hey
+ * that's the best way to get used to it, plus my brain thinks
+ * that way...so...
+ * 
+ * HERE WE GO.
+ */
+public class chessPiece {
+    // Every piece has 1 color, so it goes in the superclass.
+    String color;
+    // Every piece has 1 starting position, so it also goes in superclass.
+    String startPos;
+}
---
+++ src/Patches/Week1/Day1_Exercise-2/knightPiece.java
@@ -0,0 +1,9 @@
+// Read comment from superclass "chessPiece"
+public class knightPiece {
+    // forward and side movement is piece dependant, so..here you go.
+    // Also set the color and starting position. Dunno why...
+    int forwardMove = 3;
+    int sideMove = 2;
+    chessPiece.color = "white";
+    chessPiece.startPos = "b1g1";
+}
