aa67b83ea118421899637354abad0f497c138e9c
Patches S <teh.bloo.box@gmail.com>
2011-11-02 19:36:02 -0500

    Made Directory "Day 2", added Variables. Book said to compile it.

---
+++ src/Patches/Week1/Day2/Variables.java
@@ -0,0 +1,15 @@
+package Patches.Week1.Day2;
+// Developed by: Robert "Patches" S. <teh.bloo.box@gmail.com>
+
+public class Variables {
+        public static void main(String[] arguments) {
+                final char UP = 'U';
+                byte initialLevel = 12;
+                short location = 13250;
+                int score = 3500100;
+                boolean newGame = true;
+                
+                System.out.println("Level: " + initialLevel);
+                System.out.println("Up: " + UP);
+        }
+}
